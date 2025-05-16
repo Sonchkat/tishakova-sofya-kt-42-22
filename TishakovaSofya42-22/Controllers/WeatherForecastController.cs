6ef74cc16ce5a8f797ee34993e8b6e6bf650492f
tishakova-sofya-kt-42-22 <sonatihsakova@gmail.com>
2025-05-16 15:05:47 +0300

    add logging and WeatherForecast test request

--- TishakovaSofya42-22/Controllers/WeatherForecastController.cs
+++ TishakovaSofya42-22/Controllers/WeatherForecastController.cs
@@ -1,32 +1,49 @@
+using System.Net;
 using Microsoft.AspNetCore.Mvc;
+using TishakovaSofya42_22;
 
-namespace TishakovaSofya42_22.Controllers;
-
-[ApiController]
-[Route("[controller]")]
-public class WeatherForecastController : ControllerBase
+namespace TishakovaSofya42_22.Controllers
 {
-    private static readonly string[] Summaries = new[]
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
     {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
-
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
-
-    [HttpGet(Name = "GetWeatherForecast")]
-    public IEnumerable<WeatherForecast> Get()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        private static readonly string[] Summaries = new[]
         {
-            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            TemperatureC = Random.Shared.Next(-20, 55),
-            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-        })
-        .ToArray();
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmly", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogError("Method was called");
+
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpPost(Name = "AddNewSummary")]
+
+        public string[] AddNewSummary(string newSummary)
+        {
+            _logger.LogError("New method was called");
+
+            var list = Summaries.ToList();
+            list.Add(newSummary);
+            return list.ToArray();
+        }
     }
 }
--- TishakovaSofya42-22/WeatherForecast.cs
+++ TishakovaSofya42-22/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace TishakovaSofya42_22
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF { get; set; }
 
         public string? Summary { get; set; }
     }
